e33ab82dd8d7bf5f12bf3dab3865670cf072992f
Ivan Brambarov <ivan.brambarov@gmail.com>
2022-12-25 13:08:48 +0200

    Add Tutorial: Create a web API with ASP.NET Core from https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-5.0&tabs=visual-studio#create-a-web-project

---
+++ ToDoAPI/ToDoAPI/Models/ToDoItemDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoAPI.Models
+{
+    public class ToDoItemDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
