62bb413be68adad72a3839012330478861258342
Brambarov <ivan.brambarov@gmail.com>
2024-02-04 12:12:43 +0200

    Add CAD.NET TestProject1 solution & project

---
+++ CAD.NET/TestProject1/TestProject1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+using Teigha.Runtime;
+#if ACAD
+#endif
+#if BCAD
+#endif
+
+namespace TestProject1
+{
+    public class Class1
+    {
+        [CommandMethod("MyCommand")]
+        public void MyFirstCommand()
+        {
+            MessageBox.Show("Command succeeded!");
+        }
+    }
+}
