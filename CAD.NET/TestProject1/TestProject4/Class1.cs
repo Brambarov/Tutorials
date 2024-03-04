5e1cb5d2d33b2b77707dca084a1bcf150cf2af73
Brambarov <ivan.brambarov@gmail.com>
2024-03-04 10:47:03 +0200

    Add TestProject4 project

---
+++ CAD.NET/TestProject1/TestProject4/Class1.cs
@@ -0,0 +1,80 @@
+﻿using Bricscad.ApplicationServices;
+using Teigha.DatabaseServices;
+using Teigha.Geometry;
+using Teigha.Runtime;
+
+namespace TestProject4
+{
+    public class Class1
+    {
+        [CommandMethod("TEST4")]
+        public void TEST4()
+        {
+            // Get the current database and start a transaction
+            Database acCurDb;
+            acCurDb = Application.DocumentManager.MdiActiveDocument.Database;
+
+            acCurDb.ReadDwgFile("C:\\Users\\Ivan\\source\\repos\\Tutorials\\CAD.NET\\TestProject1\\TestProject4\\bin\\Debug\\TestBlock.dwg",
+                                FileOpenMode.OpenForReadAndReadShare,
+                                false,
+                                null);
+
+            using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+            {
+                // Open the Block table for read
+                BlockTable acBlkTbl;
+                acBlkTbl = acTrans.GetObject(acCurDb.BlockTableId, OpenMode.ForRead) as BlockTable;
+
+                ObjectId blkRecId = ObjectId.Null;
+
+                if (!acBlkTbl.Has("CircleBlock"))
+                {
+                    using (BlockTableRecord acBlkTblRec = new BlockTableRecord())
+                    {
+                        acBlkTblRec.Name = "CircleBlock";
+
+                        // Set the insertion point for the block
+                        acBlkTblRec.Origin = new Point3d(0, 0, 0);
+
+                        // Add a circle to the block
+                        using (Circle acCirc = new Circle())
+                        {
+                            acCirc.Center = new Point3d(0, 0, 0);
+                            acCirc.Radius = 2;
+
+                            acBlkTblRec.AppendEntity(acCirc);
+
+                            acTrans.GetObject(acCurDb.BlockTableId, OpenMode.ForWrite);
+                            acBlkTbl.Add(acBlkTblRec);
+                            acTrans.AddNewlyCreatedDBObject(acBlkTblRec, true);
+                        }
+
+                        blkRecId = acBlkTblRec.Id;
+                    }
+                }
+                else
+                {
+                    blkRecId = acBlkTbl["CircleBlock"];
+                }
+
+                // Insert the block into the current space
+                if (blkRecId != ObjectId.Null)
+                {
+                    using (BlockReference acBlkRef = new BlockReference(new Point3d(0, 0, 0), blkRecId))
+                    {
+                        BlockTableRecord acCurSpaceBlkTblRec;
+                        acCurSpaceBlkTblRec = acTrans.GetObject(acCurDb.CurrentSpaceId, OpenMode.ForWrite) as BlockTableRecord;
+
+                        acCurSpaceBlkTblRec.AppendEntity(acBlkRef);
+                        acTrans.AddNewlyCreatedDBObject(acBlkRef, true);
+                    }
+                }
+
+                // Save the new object to the database
+                acTrans.Commit();
+
+                // Dispose of the transaction
+            }
+        }
+    }
+}
